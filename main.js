b13bb2313bbf7d51c776d06291f04251a854f5a7
Brandon Jones <bdjones2410@gmail.com>
2016-06-23 00:04:09 -0400

    neighbors function smarter with start and end points for columns

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const randomizer = () => Math.floor(Math.random()*2)
 
+const MATRIX_SIZE = 15;
+
 const createMatrix = function(num){
     let retArr = [];
     for(var i = 0; i < num; i++){
@@ -12,141 +14,99 @@ const createMatrix = function(num){
     return retArr;
 }
 
-var prevState = createMatrix(20);
-var nextState = createMatrix(20);
+var prevState = [];
+var nextState = createMatrix(MATRIX_SIZE);
+
+const getPoints = function(row, column){
+  var startRow = 0;
+  var startColumn = 0;
+  var endRow = 0;
+  var endColumn = 0;
+
+  if(row === 0){
+    startRow = 0;
+    endRow = row + 1;
+  }
+  else if(row === MATRIX_SIZE -1){
+    startRow = row - 1;
+    endRow = row;
+  }else{
+    startRow = row -1;
+    endRow = row + 1;
+  }
+
+  if(column === 0){
+    startColumn = 0;
+    endColumn = column + 1;
+  }
+  else if(column === MATRIX_SIZE -1){
+    startColumn = column - 1;
+    endColumn = column;
+  }
+  else{
+    startColumn = column - 1;
+    endColumn = column  + 1;
+  }
+
+ getNeighbors(row, column, startRow, startColumn, endRow, endColumn)
+}
 
 
+const getNeighbors = function(row, column, startRow, startColumn, endRow, endColumn){
+  let neighbors = 0;
+  for(var i = startRow; i <= endRow; i++){
+    for(var j = startColumn; j <= endColumn; j++){
+      if(i === row && j === column){
 
-//reformat this to use more parameters,  start row, end row, start col, end col boil down to one function with loops.
-
-const getNeighbors = function(row, column){
-  if(row == 0 || column == 0){
-    if(row == 0 && column == 0){
-      bothStart(row, column)
-    }else if(row == 0){
-      if(column === (prevState[row].length -1)){
-      //  console.log("column end row 0",row, column);
-      }else {
-      //  console.log("row 0",row, column);
       }
-    }else{
-    //  console.log("col 0",row, column);
+      else{
+        if(prevState[i][j] === 1){
+          neighbors++;
+        }
+      }
     }
   }
-  else if(row === (prevState.length -1) || column === (prevState[row].length -1)){
-    if(row === (prevState.length -1) && column === (prevState[row].length -1)){
-     bothEnd(row, column);
-    }else if(row === (prevState.length -1)){
-    //  console.log("row end", row, column);
-    }else {
-  //    console.log("column end", row, column);
+
+  if(prevState[row][column] === 0){
+    if(neighbors == 3){
+      nextState[row][column] = 1;
+    }
+    else {
+      nextState[row][column] = 0;
     }
   }
   else {
-    getAll(row, column);
+    if(neighbors < 2 || neighbors > 3){
+      nextState[row][column] = 0;
+    }
+    else {
+      nextState[row][column] = 1;
+    }
+  }
+}
+
+const printStates = function(){
+  for(var k in prevState){
+    console.log(prevState[k]);
+  }
+  console.log("------------------");
+  for(var l in nextState){
+    console.log(nextState[l])
   }
 }
 
 
-const bothStart = function(row, column) {
-  let neighbors = 0;
-  let startRow = row;
-  let startColumn = column;
+const begin = function(){
+  prevState = nextState;
+  nextState = [];
+  for(var i in prevState){
+    nextState.push([]);
+  }
 
-  while(startRow < row + 2){
-    if(startRow == row && startColumn == column){
-      startColumn ++;
-    }
-    else if(startColumn > column + 1){
-      startColumn = column;
-      startRow++;
-    }
-    else{
-      startColumn++
-      if(prevState[startRow][startColumn] == 1){
-        neighbors++;
-
-      }
+  for(var i = 0; i < prevState.length; i++){
+    for(var j = 0; j < prevState[i].length; j++){
+      getPoints(i, j);
     }
   }
-  console.log("start", neighbors);
-}
-
-const rowZero = function(row, column){
-
-}
-
-const colZero = function(row, column){
-
-}
-
-const colZeroRowEnd = function(row, column) {
-
-}
-
-const rowZeroColEnd = function(row, column) {
-
-}
-
-const rowEnd = function(row, column){
-
-}
-
-const colEnd = function(row, column){
-
-}
-
-const bothEnd = function(row, column){
-  let neighbors = 0;
-  let startRow = (row - 1);
-  let startColumn = (column - 1);
-  while(startRow < row + 1){
-    if(startRow == row && startColumn == column){
-      startColumn ++;
-    }
-    else if(startColumn > column){
-      startColumn = column - 1;
-      startRow++;
-    }
-    else{
-      startColumn++
-      if(prevState[startRow][startColumn] == 1){
-        neighbors++;
-      }
-    }
-  }
-  console.log("end", neighbors);
-}
-
-const getAll = function(row, column){
-  let neighbors = 0;
-  let startRow = (row - 1);
-  let startColumn = (column - 1);
-  while(startRow < row + 2){
-    if(startRow == row && startColumn == column){
-      startColumn ++;
-    }
-    else if(startColumn > column + 1){
-      startColumn = column - 1;
-      startRow++;
-    }
-    else{
-      startColumn++
-      if(prevState[startRow][startColumn] == 1){
-        neighbors++;
-      }
-    }
-  }
-  console.log("mid", neighbors);
-}
-
-
-
-
-
-
-for(var i = 0; i < prevState.length; i++){
-  for(var j = 0; j < prevState[i].length; j++){
-    getNeighbors(i, j);
-  }
+  printStates();
 }
